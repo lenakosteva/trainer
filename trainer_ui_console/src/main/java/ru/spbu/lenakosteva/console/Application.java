619572d44ebd27207919bca62dd34654df098dad
Elena <elenakosteva04@gmail.com>
2025-03-29 13:27:42 +0300

    homework 8 (cleanup)

--- trainer_ui_console/src/main/java/ru/spbu/lenakosteva/console/Application.java
+++ trainer_ui_console/src/main/java/ru/spbu/lenakosteva/console/Application.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.spbu.lenakosteva.console.config.SpringConfig;
 import ru.spbu.lenakosteva.console.controller.ConsoleController;
 
 @SpringBootApplication
--- trainer_ui_swing/src/main/java/ru/spbu/lenakosteva/gui/Application.java
+++ trainer_ui_swing/src/main/java/ru/spbu/lenakosteva/gui/Application.java
@@ -4,21 +4,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.spbu.lenakosteva.domain.service.QuestionService;
-import ru.spbu.lenakosteva.gui.config.SpringConfig;
 import ru.spbu.lenakosteva.gui.controller.MainController;
 
 import javax.swing.*;
 
 @SpringBootApplication
-public class Main  {
-    private static final Logger logger = LogManager.getLogger(Main.class);
+public class Application  {
+    private static final Logger logger = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Main.class)
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class)
                 .headless(false).run(args);
         logger.info("Начало работы приложения");
         QuestionService questionService = ctx.getBean(QuestionService.class);
